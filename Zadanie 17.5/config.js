b10797200d73834ef7f2408c11526561ccdc708f
unknown <asiac00@gmail.com>
2018-01-18 11:01:42 +0100

    minor fixes in task 4 and 5

--- Zadanie 17.5/config.js
+++ Zadanie 17.5/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'GOOGLE_CLIENT_ID':'1096460933644-edee71drcffjsfq68hq9tv64d5n66a22.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET':'9H4O8WLqZ7IyLgxMg0p50m8S',
+    'GOOGLE_CLIENT_ID':'915081615804-t862i96l3rg7aius6qndj3s9q865hgj5.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET':'kf6jfKSWHf3FDv4ucXidzRtj',
     'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
 }
